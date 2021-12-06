7213c66e8c9c6885c51213bfae1472f5399d33b5
tbiasi <tr.biasi@gmail.com>
2021-12-06 11:22:35 -0500

    experiments finished

--- experiments.py
+++ experiments.py
@@ -59,38 +59,36 @@ def run_experiment(number_of_repetitions,
 number_of_repetitions = 5
 time_limit = 20                 # run no longer than 10 units of time
 pair_arrival_rate = 100         # means that 100 pairs are going to be arriving every one time period
-base_pair_departure_rate = 0.5       # means that each pairs is expected to last 2 units of time
+base_pair_departure_rate = 0.4      # means that each pairs is expected to last 2.25 units of time
 base_altruist_arrival_rate = 1.0     # means 1 altruist in expectation will show up each unit of time
 
-run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate = base_pair_departure_rate, altruist_arrival_rate = 1, batch_size=10)
+
+# Batch size experiments
+batch_sizes = [10, 20, 30, 50, 100, 1]
+for batch_size in batch_sizes:
+    run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=base_pair_departure_rate, batch_size=batch_size)
 
 
-# # Batch size experiments
-# batch_sizes = [10, 20, 30, 50, 100, 1]
-# for batch_size in batch_sizes:
-#     run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=0.5, batch_size=batch_size)
+# Pair departure rates experiments
+departure_rates = [0.2, 0.4, 0.6, 0.8]
+batch_sizes = [10, 20, 30, 50, 100, 1]
+for batch_size in batch_sizes:
+    for departure_rate in departure_rates:
+        run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=departure_rate, batch_size=batch_size)
 
 
-# # Pair departure rates experiments
-# departure_rates = [0.2, 0.4, 0.6, 0.8]
-# batch_sizes = [10, 20, 30, 50, 100, 1]
-# for batch_size in batch_sizes:
-#     for departure_rate in departure_rates:
-#         run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=departure_rate, batch_size=batch_size)
+# Impact of altruists
+altruist_arrival_rates = [0.5, 1.0]
+for batch_size in batch_sizes:
+    for altruist_arrival_rate in altruist_arrival_rates:
+        run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=base_pair_departure_rate, altruist_arrival_rate=altruist_arrival_rate, batch_size=batch_size)
 
 
-# # Impact of altruists
-# altruist_arrival_rates = [0.5, 1.0]
-# for batch_size in batch_sizes:
-#     for altruist_arrival_rate in altruist_arrival_rates:
-#         run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=base_pair_departure_rate, altruist_arrival_rate=altruist_arrival_rate, batch_size=batch_size)
-
-
-# # Potential and Fairness Weighted
-# for solver_type in [ProblemType.POTENTIALS, ProblemType.FAIRNESS]:
-#     for batch_size in [1, 10, 30]:
-#         run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=base_pair_departure_rate, 
-#                         altruist_arrival_rate=base_altruist_arrival_rate, batch_size=batch_size, problem_type=solver_type)
+# Potential and Fairness Weighted
+for solver_type in [ProblemType.POTENTIALS, ProblemType.FAIRNESS]:
+    for batch_size in [1, 10, 30]:
+        run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=base_pair_departure_rate, 
+                        altruist_arrival_rate=base_altruist_arrival_rate, batch_size=batch_size, problem_type=solver_type)
 
 
 
