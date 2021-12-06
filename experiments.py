35aa3bd7ac478c129dc8bbee8601f55743740405
tbiasi <tr.biasi@gmail.com>
2021-12-06 11:09:54 -0500

    experiments written up

--- experiments.py
+++ experiments.py
@@ -6,10 +6,10 @@ import random
 import time
 
 def run_experiment(number_of_repetitions,
-                  pair_arrival_rate, pair_departure_rate,
+                  time_limit,
+                  pair_arrival_rate, pair_departure_rate=0,
                   altruist_arrival_rate=0, altruist_departure_rate=0,
-                  problem_type=ProblemType.SIMPLE, batch_size=10,
-                  time_limit=10):
+                  problem_type=ProblemType.SIMPLE, batch_size=10):
     start_time = time.time()
 
     averaged_statistics = {}
@@ -31,6 +31,18 @@ def run_experiment(number_of_repetitions,
         averaged_statistics[key] /= number_of_repetitions
     
     print()
+    print()
+    print("EXPERIMENT HYPERPARAMETERS")
+    print("Number of reps:", number_of_repetitions)
+    print("Time limit:", time_limit)
+    print("Pair arrival rate:", pair_arrival_rate)
+    print("Pair departure rate:", pair_departure_rate)
+    print("Altruist arrival rate:", altruist_arrival_rate)
+    print("Altruist departure rate:", altruist_departure_rate)
+    print("Problem Type:", problem_type)
+    print("Batch size:", batch_size)
+
+    print()
     print("EXPERIMENT RESULTS")
     print(f"{number_of_repetitions} Repetitions")
     print()
@@ -43,7 +55,48 @@ def run_experiment(number_of_repetitions,
     print(f"Total Time for Experiment: {round((end_time - start_time) / 60, 3)} minutes")
 
 
-# Example experiment
-run_experiment(5, 100, 1, time_limit=20)
+# Baseline hyperparameters
+number_of_repetitions = 5
+time_limit = 20                 # run no longer than 10 units of time
+pair_arrival_rate = 100         # means that 100 pairs are going to be arriving every one time period
+base_pair_departure_rate = 0.5       # means that each pairs is expected to last 2 units of time
+base_altruist_arrival_rate = 1.0     # means 1 altruist in expectation will show up each unit of time
+
+run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate = base_pair_departure_rate, altruist_arrival_rate = 1, batch_size=10)
+
+
+# # Batch size experiments
+# batch_sizes = [10, 20, 30, 50, 100, 1]
+# for batch_size in batch_sizes:
+#     run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=0.5, batch_size=batch_size)
+
+
+# # Pair departure rates experiments
+# departure_rates = [0.2, 0.4, 0.6, 0.8]
+# batch_sizes = [10, 20, 30, 50, 100, 1]
+# for batch_size in batch_sizes:
+#     for departure_rate in departure_rates:
+#         run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=departure_rate, batch_size=batch_size)
+
+
+# # Impact of altruists
+# altruist_arrival_rates = [0.5, 1.0]
+# for batch_size in batch_sizes:
+#     for altruist_arrival_rate in altruist_arrival_rates:
+#         run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=base_pair_departure_rate, altruist_arrival_rate=altruist_arrival_rate, batch_size=batch_size)
+
+
+# # Potential and Fairness Weighted
+# for solver_type in [ProblemType.POTENTIALS, ProblemType.FAIRNESS]:
+#     for batch_size in [1, 10, 30]:
+#         run_experiment(number_of_repetitions, time_limit, pair_arrival_rate, pair_departure_rate=base_pair_departure_rate, 
+#                         altruist_arrival_rate=base_altruist_arrival_rate, batch_size=batch_size, problem_type=solver_type)
+
+
+
+
+
+
+
 
 
